5d5d2942e71f708d2898cf0f6591b76daab55232
Zeinekken-hub <bogdanmuha5@gmail.com>
2020-10-04 22:33:12 +0300

    init

---
+++ MqttConsoleClient/MqttConsoleClient/MqttConsoleClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+namespace MqttConsoleClient
+{
+    public class MqttConsoleClient
+    {
+        private readonly IMqttClient _client;
+        private readonly IMqttClientOptions _clientOptions;
+
+        public MqttConsoleClient(IMqttClientOptions clientOptions)
+        {
+            _clientOptions = clientOptions;
+            _client = new MqttFactory().CreateMqttClient();
+            RegisterHandlers();
+        }
+
+        public async Task PublishAsync(MqttApplicationMessage message)
+        {
+            await _client.PublishAsync(message);
+        }
+
+        public async Task StartAsync()
+        {
+            await _client.ConnectAsync(_clientOptions, CancellationToken.None);
+        }
+
+        public async Task SubscribeAsync(params string[] topics)
+        {
+            await Task.WhenAll(topics.Select(async topic => await _client.SubscribeAsync(topic)));
+
+            Console.WriteLine($"Subscribed to {string.Join(',', topics)} topics");
+        }
+
+        public async Task DisconnectAsync()
+        {
+            await _client.DisconnectAsync();
+        }
+
+        private void RegisterHandlers()
+        {
+            _client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("### Connected with server ###");
+            });
+
+            _client.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine();
+                Console.WriteLine("# Received application message #");
+                Console.WriteLine($"From topic: {e.ApplicationMessage.Topic}");
+                Console.WriteLine($"Payload: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+                Console.WriteLine($"Qos: {e.ApplicationMessage.QualityOfServiceLevel}");
+                Console.WriteLine($"Retain: {e.ApplicationMessage.Retain}");
+                Console.WriteLine();
+            });
+
+            _client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("### Disconnected from server ###");
+            });
+        }
+    }
+}
---
+++ MqttConsoleClient/MqttConsoleClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Options;
+
+namespace MqttConsoleClient
+{
+    internal class Program
+    {
+        private static async Task Main()
+        {
+            var client = new MqttConsoleClient(GetConsoleInputOptions());
+
+            await client.StartAsync();
+
+            await client.SubscribeAsync("hello/world", "hello/test");
+
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                await client.PublishAsync(GetConsoleInputMessage());
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            await client.DisconnectAsync();
+        }
+
+        private static MqttApplicationMessage GetConsoleInputMessage()
+        {
+            return new MqttApplicationMessageBuilder()
+                .WithExactlyOnceQoS()
+                .WithPayload(ReadFromConsole("Message: "))
+                .WithTopic(ReadFromConsole("Topic: "))
+                .Build();
+        }
+
+        private static IMqttClientOptions GetConsoleInputOptions(string server = "localhost", int port = 1883)
+        {
+            return new MqttClientOptionsBuilder()
+                .WithClientId(ReadFromConsole("Client id: "))
+                .WithTcpServer(server, port)
+                .Build();
+        }
+
+        private static string ReadFromConsole(string label = "")
+        {
+            Console.Write(label);
+            return Console.ReadLine();
+        }
+    }
+}
